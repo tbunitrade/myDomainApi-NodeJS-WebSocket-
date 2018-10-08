38d74a7f35f3d960dc6bf13ac93d6a211be1d724
tbunitrade <tbunitrade@gmail.com>
2018-10-08 09:01:34 +0300

    first commit

---
+++ samplenode.js
@@ -0,0 +1,17 @@
+1+2
+var foo = 'bond009'
+var mycat = 'cat'
+foo
+mycat
+1 + 2 + 3 + (1+1 )
+1 + 2 + 3 * (1+1 )
+1 + 2 + 3 * (1*1 )
+1 + 2 + 3 * (1*2 )
+_ + 100
+var myfunction = function () { return "hello world"};
+myfunction()
+var myCity = function() {
+
+return ('Kiev')
+}
+myCity()
